0009b29cb745e66d63f3cf770d7c8bf17ad563ef
CMDeve <mcuong1407@gmail.com>
2024-03-07 02:39:52 +0700

    cursor mouse'

---
+++ CursorMouse.h
@@ -0,0 +1,27 @@
+#ifndef AIMMOUSE_H
+#define AIMMOUSE_H
+
+#include "mainData.h"
+
+void loadCursorMouseIMG()
+{
+    //image: https://www.pngegg.com/
+    const char* path = (imagePath + "aim0.png").c_str();
+    SDL_Surface* cursorSurface = IMG_Load(path);
+    if (cursorSurface == NULL) {
+        printf("Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError());
+        return;
+    }
+
+    SDL_SetColorKey(cursorSurface, SDL_TRUE, SDL_MapRGB(cursorSurface->format, 255, 255, 255));
+    SDL_Cursor* cursor = SDL_CreateColorCursor(cursorSurface, 0, 0);
+    if (cursor == NULL) {
+        printf("Failed to create custom cursor! SDL Error: %s\n", SDL_GetError());
+        return;
+    }
+
+    SDL_SetCursor(cursor);
+    SDL_FreeSurface(cursorSurface);
+}
+
+#endif
