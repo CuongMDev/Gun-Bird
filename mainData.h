8eff573cf677b0f6a7a84e496917ed9b39a42ca2
CMDeve <mcuong1407@gmail.com>
2024-03-07 02:41:05 +0700

    main data

---
+++ mainData.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_DATA_H
+#define MAIN_DATA_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 621;
+
+int gVelocityYScene = 10;
+
+const std::string imagePath = "image/";
+
+SDL_Window* gWindow = NULL;
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+#endif
