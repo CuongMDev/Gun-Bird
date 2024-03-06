df443bb4dd0b9d16d25ef327a186bc70b1b13677
CMDeve <mcuong1407@gmail.com>
2024-03-07 00:10:08 +0700

    Removed folder_name from repository

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-#include <SDL2/SDL.h>
-#include <iostream>
+#include "mainData.h"
+
+void close()
+{
+    //Destroy window	
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    gRenderer = NULL;
+
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+}
 
 int main(int argc, char* args[])
 {
+    //Start up SDL and create window
+    if (!init()) {
+        printf("Failed to initialize!\n");
+        return 0;
+    }
+
 
 
     return 0;
