f52ecf841dbc2477ea5ac39b15f40144c2aef4a7
CMDeve <mcuong1407@gmail.com>
2024-03-07 00:12:01 +0700

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mainData.h"
+#include "objects.h"
 
 void close()
 {
@@ -18,10 +19,47 @@ int main(int argc, char* args[])
     //Start up SDL and create window
     if (!init()) {
         printf("Failed to initialize!\n");
-        return 0;
+    }
+    else {
+        //Main loop flag
+        bool quit = false;
+        //Event handler
+        SDL_Event e;
+
+        Background background;
+        Ground ground(groundPosX, groundPosY);
+        MainBird mainBird(mainBirdPosX, mainBirdPosY);
+        while (!quit)
+        {
+            //Clear screen
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL_RenderClear(gRenderer);
+
+            //Handle events on queue
+            while (SDL_PollEvent(&e) != 0) {
+                //User requests quit
+                if (e.type == SDL_QUIT) {
+                    quit = true;
+                }
+
+                //Handle input for the Main Bird
+                mainBird.handleEvent(e);
+            }
+
+            mainBird.move(ground);
+
+            //Render
+            background.render();
+            ground.render();
+            mainBird.render();
+
+            //Update screen
+            SDL_RenderPresent(gRenderer);
+        }
     }
 
-
+    //Free resources and close SDL
+    close();
 
     return 0;
 }
