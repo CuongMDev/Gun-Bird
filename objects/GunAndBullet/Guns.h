af15cf75a174d925700b81d58b1501efb9e4649a
CMDeve <mcuong1407@gmail.com>
2024-03-17 16:00:45 +0700

    Update gun

--- objects/GunAndBullet/Guns.h
+++ objects/GunAndBullet/Guns.h
@@ -9,31 +9,57 @@
 const int gunPosX = 5;
 const int gunPosY = groundPosY + 10;
 
+enum GUN_TYPE
+{
+    PISTOL,
+    SILENT_PISTOL,
+    GOLD_PISTOL,
+    AK47,
+    SNIPER,
+
+    GUN_COUNT
+};
+
+struct GunProperties
+{
+    int damage;
+    int timeRestoreAim;
+    int recoil;
+    int maxVelRecoil;
+    int shootDelay;
+
+    std::string imageName;
+};
+
 class Guns : private Object
 {
 private:
+    static const GunProperties gunProperties[GUN_COUNT];
+    LTexture sTexture[GUN_COUNT];
+
     ObjectsList bulletsList;
+    GUN_TYPE currentGun;
 
     //The angle
     double mAngle;
 
-    //shot when curTime=speed
+    //shot when curTime=shotDelay
     int curTime;
-    int shootDelay;
+
+    int curTimeRestoreAim;
 
     int mVelRecoil;
-    const int maxVelRecoil = 10;
 
     bool mouseHold;
 
     void loadGunIMG();
     void turnTowards(int mouseX, int mouseY);
     void updateAngle();
+    void restoreAim();
     //check and shoot
     void shoot();
 
 public:
-
     Guns();
 
     ObjectsList &getBulletList();
@@ -44,7 +70,15 @@ public:
     void setPosAndAngle(int x, int y);
 };
 
-Guns::Guns()
+const GunProperties Guns::gunProperties[] = {
+        {3, 17, 1, 3, 15, "pistol.png"}, //pistol
+        {3, 17, 1, 3, 10, "silentpistol.png"}, //silent pistol
+        {7, 17,1,  5, 15, "goldpistol.png"}, //gold pistol
+        {5, 7, 1, 7, 5, "ak47.png"}, //AK47
+        {12, 30, 20, 20, 30, "sniper.png"}, //Sniper
+};
+
+Guns::Guns() : Object(false)
 {
     loadGunIMG();
     init();
@@ -61,8 +95,10 @@ void Guns::init()
     mPosY = 0;
     mAngle = 0;
     mVelRecoil = 0;
-    shootDelay = 5;
     mouseHold = false;
+
+    currentGun = SNIPER;
+    mTexture = &sTexture[currentGun];
 
     bulletsList.reset();
 }
@@ -70,7 +106,9 @@ void Guns::init()
 void Guns::loadGunIMG()
 {
     //image: https://midnitepixelated.itch.io/pixel-guns
-    mTexture->loadFromFile(gunImagePath + "ak47.png", true, 67, 76, 111);
+    for (auto gunType = 0; gunType < GUN_COUNT; gunType++) {
+        sTexture[gunType].loadFromFile(gunImagePath + gunProperties[gunType].imageName, true, 67, 76, 111);
+    }
 }
 
 void Guns::turnTowards(int mouseX, int mouseY)
@@ -89,32 +127,59 @@ void Guns::updateAngle()
     turnTowards(mouseX, mouseY);
 }
 
+void Guns::restoreAim()
+{
+    if (curTimeRestoreAim > 0) {
+        curTimeRestoreAim--;
+        if (curTimeRestoreAim % gunProperties[currentGun].timeRestoreAim == 0) {
+            CursorMouse::move(0, 10);
+        }
+    }
+
+    if (curTimeRestoreAim % gunProperties[currentGun].timeRestoreAim == 0) {
+        if (mVelRecoil > 0) {
+            //restore gun aim
+            mVelRecoil--;
+        }
+    }
+}
+
+
 void Guns::shoot()
 {
+    if (curTime < gunProperties[currentGun].shootDelay) {
+        curTime++;
+    }
+
     if (!mouseHold) {
-        if (mVelRecoil > 0) {
-            mVelRecoil--;
-            //restore gun aim
-            CursorMouse::move(0, 2);
-        }
+        restoreAim();
         return;
     }
 
-    curTime++;
-    if (curTime < shootDelay) {
+    if (curTime < gunProperties[currentGun].shootDelay) {
         //It's not time to shoot yet
         return;
     }
 
-    if (mVelRecoil < maxVelRecoil) {
-        mVelRecoil++;
+    //add bullet
+    bulletsList.add(new Bullets(mPosX, mPosY, mAngle));
+
+    //increase time restore aim
+    if (curTimeRestoreAim < gunProperties[currentGun].timeRestoreAim * 2) {
+        curTimeRestoreAim += gunProperties[currentGun].timeRestoreAim;
     }
+
+    mVelRecoil = std::max(mVelRecoil + gunProperties[currentGun].recoil, gunProperties[currentGun].maxVelRecoil);
     //reset
     curTime = 0;
     //recoil
-    CursorMouse::recoilMouse(curTime, 10 * std::max(0, mVelRecoil - 3));
-    //add bullet
-    bulletsList.add(new Bullets(mPosX, mPosY, mAngle));
+    if (mVelRecoil < 3) {
+        //3 first bullets have little recoil
+        CursorMouse::recoilMouse(1, 10);
+    }
+    else {
+        CursorMouse::recoilMouse(1, 10 * mVelRecoil);
+    }
 }
 
 void Guns::handleEvent(SDL_Event *e)
